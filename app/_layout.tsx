f4ef0a27d59f49e72aebffeb3cf631598653dc5b
martinalmeida <martinalmeida56@gmail.com>
2025-06-23 16:32:33 -0500

    feat: :tada: configuracion inicial con tailwind

---
+++ app/_layout.tsx
@@ -0,0 +1,6 @@
+import { Stack } from "expo-router";
+import "../assets/css/global.css";
+
+export default function RootLayout() {
+  return <Stack />;
+}
---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-green-500">
+      <Text className="text-xl font-bold text-blue-500">Prueba</Text>
+    </View>
+  );
+}
